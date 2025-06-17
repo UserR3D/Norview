29d9b1c9c983dd470e15b0bfa6a2e0cb0a327dd4
red <kauawar04@gmail.com>
2025-06-17 10:12:35 -0300

    login with localstorage add

---
+++ src/app/login/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { use } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  const [user, setUser] = React.useState<string>("");
+  const [access, setAccess] = React.useState<string>("");
+
+  async function login() {
+    const request = await fetch("http://localhost:3333/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: user, password: access }),
+    });
+    const response = await request.json();
+    window.localStorage.setItem("token", response.token)
+  }
+  console.log(user, access);
+
+  return (
+    <div className={styles.page}>
+      <input
+        type="email"
+        value={user}
+        onChange={(e) => {
+          setUser(e.target.value);
+        }}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={access}
+        onChange={(e) => {
+          setAccess(e.target.value);
+        }}
+        placeholder="Password"
+      />
+      <button onClick={login}>Enter</button>
+    </div>
+  );
+}
