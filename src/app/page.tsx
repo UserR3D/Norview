513f1f695abbc698c2b9fbe5eaab833296534f8c
red <kauawar04@gmail.com>
2025-06-23 00:59:25 -0300

    posts page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import React from "react";
-import { Author } from "./author/user";
 
-interface Post {
+interface Post extends Author {
   id: number;
   title: string;
   content: string;
@@ -12,27 +10,27 @@ interface Post {
   authorID: number;
 }
 
-export default function Home() {
-  const [post, setPost] = React.useState<Post[]>();
+type Author = {
+  author: {
+    email: string;
+    id: number;
+    role: "ADMIN" | "USER";
+  };
+};
 
-  React.useEffect(() => {
-    async function posts() {
-      const request = await fetch("http://localhost:3333/user/posts");
-      const response = (await request.json()) as Post[];
-      setPost(response);
-    }
-    posts();
-  }, []);
-
-  if (!post) return <p>Can't connect</p>;
+export default async function Home() {
+  const request = await fetch("http://localhost:3333/user/posts");
+  const posts = (await request.json()) as Post[];
+  console.log(posts);
+  if (!posts) return <p>Can't connect</p>;
 
   return (
     <main className={styles.main}>
-      {post.map((item) => (
+      {posts.map((item) => (
         <div key={item.id}>
           <h2>{item.title}</h2>
           <p>{item.content}</p>
-          <Author id={item.authorID} />
+          <h4>{item.author.email}</h4>
         </div>
       ))}
     </main>
