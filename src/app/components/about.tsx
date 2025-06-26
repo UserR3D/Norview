5f5fb20f90257a6b7019ff48f47ad052d9e0c481
red <kauawar04@gmail.com>
2025-06-26 09:07:01 -0300

    about home

---
+++ src/app/components/about.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <ul className="flex content-center items-center gap-[30px]">
+      <li className="max-w-max text-7xl text-center bg-[#D9D9D9] p-[3.5rem] ">
+        <h1>
+          Welcome <br />
+          To
+          <br /> Norview
+        </h1>
+      </li>
+      <li className="text-right text-4xl bg-[#D9D9D9] p-[3.5rem]  ">
+        <h3 className="pb-[3.5rem]">
+          This blog site was created with a simple objective:
+        </h3>
+        <h3>to give you a friendly space for leaving comments.</h3>
+      </li>
+    </ul>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Nav from "@/app/components/nav";
+import About from "@/app/components/about";
 export default function Home() {
   return (
     <div>
       <Nav />
+      <About />
     </div>
   );
 }
