f5a59301c461022d0d5d265e76ad3c0c4e2c9ee1
red <kauawar04@gmail.com>
2025-07-01 10:40:19 -0300

    chunks post

--- src/app/components/posts.tsx
+++ src/app/components/posts.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import getPosts from "../lib/getPosts";
+
+let chunkIndex = 0;
+
+const chunkSize = 3;
+const chunks: Post[][] = [];
 
 export default async function Posts() {
-  const posts = await getPosts();
+  const request = await fetch("http://localhost:3333/users/posts");
+  if (!request.ok) throw new Error("Can't connect");
+  const posts = (await request.json()) as Post[];
   if (!posts) return <p>Error</p>;
+
+  for (let i = 0; i < posts.length; i += chunkSize) {
+    const chunk = posts.slice(i, i + chunkSize);
+    chunks.push(chunk);
+  }
   return (
     <div>
       <h2>Posts</h2>
       <ul className="grid grid-cols-3 gap-[30px] [&>*]:bg-[#B3B3B3]  ">
-        {posts.map((item) => (
-          <li key={item.id} className="grid  max-w-[500px] h-[250px]">
+        {chunks[1].map((item) => (
+          <li key={item.id} className="grid max-w-[500px] h-[250px]">
             <h3>{item.title}</h3>
             <p className="text-ellipsis overflow-hidden">{item.content}</p>
-            <h3>{item.author.email}</h3>
           </li>
         ))}
       </ul>
--- src/app/lib/getPosts.ts
+++
@@ -1,6 +0,0 @@
-export default async function getPosts() {
-  const request = await fetch("http://localhost:3333/users/posts");
-  if (!request.ok) throw new Error("Can't connect");
-  const posts = (await request.json()) as Post[];
-  return posts;
-}
