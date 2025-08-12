138f6fbe7c47611dc25259462200ff1941e88bdf
red <kauawar04@gmail.com>
2025-08-12 10:40:02 -0300

    nav about responsive

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,12 +4,13 @@ export default async function About() {
   return (
     <ul
       className={`${styles.about} 
-      flex content-center container justify-center content-center items-stretch text-(--font-P) gap-2`}
+      flex content-center container justify-center content-center items-stretch text-(--font-P) gap-2
+      max-sm:flex-col
+      `}
     >
       <li
-        className="text-7xl text-center self-center
-      max-[90rem]:text-5xl
-      "
+        className="text-7xl text-center self-stretch
+        max-2xl:text-5xl max-lg:text-3xl"
       >
         <h1>
           Welcome <br />
@@ -17,7 +18,11 @@ export default async function About() {
           <br /> Norview
         </h1>
       </li>
-      <li className="text-5xl text-center">
+
+      <li
+        className="text-5xl text-center
+      max-2xl:text-3xl max-lg:text-lg"
+      >
         <h3>
           This blog site was created with a simple objective: to give you a
           friendly space for leaving comments.
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -6,13 +6,13 @@ export default function Nav({ username }: { username: string | undefined }) {
   return (
     <nav
       className={`${styles.navMain} container bg-(--bg-fgray) text-(--font-P) flex justify-between text-4xl px-[90px] py-[16px]
-      max-[90rem]:text-3xl max-[80rem]:text-2xl max-[60rem]:px-[18px] max-[43.75rem]:text-xs 
+      max-xl:text-2xl max-lg:text-3xl max-lg:px-[20px] max-sm:text-base
       `}
     >
       <Link href={"/"}>
         <h2>Norview</h2>
       </Link>
-      <ul className="flex gap-8 justify-end max-[43.75rem]:gap-2">
+      <ul className="flex gap-8 justify-end max-md:gap-2">
         <Link href={"/posts/createPost"}>
           <li>Create+</li>
         </Link>
